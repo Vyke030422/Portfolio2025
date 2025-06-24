b100eaad2e3fa587f616a4a0b1ccdef33ece01b0
Kevin Kurt Intong <kevinintong20@gmail.com>
2025-06-24 16:48:35 +0800

    New Update

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,30 +6,18 @@ export default function Navigation() {
     <nav className={style.navbar}>
       <div className={style.navbar_logo}></div>
       <ul className={style.navbar_lists}>
-        <li className="nav-item">
-          <a href="/">Home</a>
-        </li>
+       
         <li className="nav-item">
           <a href="/about">About</a>
         </li>
-        <li className="nav-item">
-          <a href="#CV">CV</a>
-        </li>
+       
         <li className="nav-item">
           <a href="#projects">Works</a>
         </li>
         <li className="nav-item">
           <a href="#contact">Contact</a>
         </li>
-        <li className={style.navbar_Linkedin}>
-          <a href="https://www.linkedin.com/in/kevin-intong-ab424b323/"><Linkedin /></a>
-        </li>
-        <li className={style.navbar_Facebook}>
-          <a href="https://www.facebook.com/profile.php?id=61572670653715"><Facebook/></a>
-        </li>
-        <li className={style.navbar_Instagram}>
-          <a href="#Instagram"><Instagram/></a>
-        </li>
+       
       </ul>
     </nav>
   );
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -2,6 +2,7 @@ import { MoveDown } from "lucide-react";
 import CardImage from "../components/cardimage";
 import Navigation from "../components/navigation";
 import style from "./homepage.module.css";
+import { Facebook, Linkedin, Instagram } from "lucide-react";
 export default function HomePage() {
   return (
     <>
@@ -16,6 +17,21 @@ export default function HomePage() {
             In every design, I aim to explore, the beauty of less, where less is
             more.
           </p>
+          <div className={style.navbar_Linkedin}>
+            <a href="https://www.linkedin.com/in/kevin-intong-ab424b323/">
+              <Linkedin />
+            </a>
+          </div>
+          <div className={style.navbar_Facebook}>
+            <a href="https://www.facebook.com/profile.php?id=61572670653715">
+              <Facebook />
+            </a>
+          </div>
+          <div className={style.navbar_Instagram}>
+            <a href="#Instagram">
+              <Instagram />
+            </a>
+          </div>
           <div className={style.MoveDown}>
             <MoveDown strokeWidth={2} />
           </div>
@@ -23,30 +39,29 @@ export default function HomePage() {
       </section>
       <section className={style.Features} id="projects">
         <CardImage
-        image="https://example.com/image1.jpg"
-        imageAlt="Project 1"
-        title="CancerLine Companion"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-      />
-      <CardImage
-        image="https://example.com/image1.jpg"
-        imageAlt="Project 2"
-        title="ITSO"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        
-      />
-      <CardImage
-        image="https://example.com/image1.jpg"
-        imageAlt="Project 3"
-        title="Island Boy"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-      />
-      <CardImage
-        image="https://example.com/image1.jpg"
-        imageAlt="Project 4"
-        title="Mighty Might Motors"
-        description="A companion app."
-      />
+          image="https://example.com/image1.jpg"
+          imageAlt="Project 1"
+          title="CancerLine Companion"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <CardImage
+          image="https://example.com/image1.jpg"
+          imageAlt="Project 2"
+          title="ITSO"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <CardImage
+          image="https://example.com/image1.jpg"
+          imageAlt="Project 3"
+          title="Island Boy"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <CardImage
+          image="https://example.com/image1.jpg"
+          imageAlt="Project 4"
+          title="Mighty Might Motors"
+          description="A companion app."
+        />
       </section>
     </>
   );
